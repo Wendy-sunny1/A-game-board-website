dc1f80b526c09399f95eface84dccca953b291d5
Wendy-sunny1 <140019411+Wendy-sunny1@users.noreply.github.com>
2024-08-04 11:43:19 +1200

    generic useData hook added

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-clients";
 import { CanceledError } from "axios";
+import useData from "./useData";
 export interface Game {
   id: number;
   name: string;
@@ -13,30 +14,5 @@ export interface Platform {
   name: string;
   slug: string;
 }
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-export const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    const controller = new AbortController();
-    setIsLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
 
-    return () => controller.abort();
-  }, []);
-  return { games, error, isLoading };
-};
+export const useGames = () => useData<Game>("/games");
