1cd4b082e461d8c5e9372f3ca7daf9c8b5c7eba9
Wendy-sunny1 <140019411+Wendy-sunny1@users.noreply.github.com>
2024-08-03 20:09:47 +1200

    GameCard component added

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-clients";
 import { CanceledError } from "axios";
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface FetchGamesResponse {
   count: number;
