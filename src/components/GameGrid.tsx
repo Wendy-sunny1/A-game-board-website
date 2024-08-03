f876f5703d5485c6acd81a49f15b92caafc19f8c
Wendy-sunny1 <140019411+Wendy-sunny1@users.noreply.github.com>
2024-08-03 19:51:23 +1200

    useGames hook added

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,24 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-clients";
 import { list, Text } from "@chakra-ui/react";
-interface Game {
-  id: number;
-  name: string;
-}
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-export const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+import { useGames } from "../hooks/useGames";
 
+export const GameGrid = () => {
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
