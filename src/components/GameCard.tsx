4b115890d986e00acec1ed762ef90ed6e8d3ca7d
Wendy-sunny1 <140019411+Wendy-sunny1@users.noreply.github.com>
2024-08-05 11:21:43 +1200

    add placeholder to games without image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,15 +1,15 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { PlatformIconList } from "./PlatformIconList";
 import { CriticScore } from "./CriticScore";
+import noImage from "../assets/place holder.png";
 interface Props {
   game: Game;
 }
 export const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={game.background_image}></Image>
+      <Image src={game.background_image || noImage}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
